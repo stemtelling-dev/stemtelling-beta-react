edb32a5a430b9d9fc31c4fcf2d12472c3f132081
Edwin Jaquez <edsmacbook@gmail.com>
2020-11-05 20:13:05 -0400

    skeleton file

---
+++ src/components/splashbanner.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class splashbanner extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default splashbanner;
---
+++ src/pages/splash/splashpage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class splashpage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default splashpage;
